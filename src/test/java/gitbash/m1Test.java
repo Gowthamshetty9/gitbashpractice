70bbe016a2d25d4c580965991458b83070865b05
Gowthamshetty9 <123gowthamac@gmail.com>
2023-06-05 11:03:39 -0300

    commit final

--- src/test/java/gitbash/m1Test.java
+++ src/test/java/gitbash/m1Test.java
@@ -4,11 +4,10 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class m1Test {
-//welcome
 	@Test
 	public void t1() {
-		// TODO Auto-generated method stub
    System.out.println("hii");
-  // Reporter.log("haii",true);
+   System.out.println("byee");
+ 
 	}
 }
